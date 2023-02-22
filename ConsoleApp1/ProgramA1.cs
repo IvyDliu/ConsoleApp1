899be370e4b3669764b0a83326b0cd34f8f3c175
IvyDliu <hirodddd@gmail.com>
2023-02-22 04:16:08 -0500

    w2hw1&2

---
+++ ConsoleApp1/ProgramA1.cs
@@ -0,0 +1,210 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace ConsoleApp1;
+
+public class HackerName
+{
+    public void AskName()
+    {
+        Console.WriteLine("What is favorite color?");
+        String color = Console.ReadLine();
+        Console.WriteLine("What is your astrology sign?");
+        String astr = Console.ReadLine();
+        Console.WriteLine("What is your street number?");
+        String street = Console.ReadLine();
+        Console.WriteLine($"Your hacker name is ... {color}{astr}{street}");
+    }
+}
+
+public class PracticeNumbers
+{
+    public void Question1()
+    {
+        String[] types = { "sbyte", "byte", "short", "ushort", "int", "uint", "long",
+            "ulong", "float", "double", "decimal"};
+        int[] sizes = { 8, 8, 16, 16, 32, 32, 64, 64, 4, 8, 16};
+        String[] mins =
+        {
+            "-128", "0", "-32768", "0", "-2147483648", "0", "-9,223,372,036,854,775,808", "0",
+            "±1.5 x 10^45", "±5.0 × 10^324", "±1.0 x 10^28"
+        };
+        String[] maxs = { "127", "255", "32767", "65535", "2,147,483,647", "4,294,967,295", 
+            "9,223,372,036,854,775,807", "18,446,744,073,709,551,615", "±3.4 x 10^38", "±1.7 × 10^308", "±7.9228 x 10^28"
+        };
+        Console.WriteLine("{0,-10} {1,-10} {2, -30} {3, -30}", "Types", "Size", "Min", "Max");
+        for (int i = 0; i < types.Length; i++)
+        {
+            Console.WriteLine("{0,-10} {1,-10} {2, -30} {3, -30}", types[i], sizes[i], mins[i], maxs[i]);
+        }
+
+    }
+
+    public void Question2()
+    {
+        Console.WriteLine("Enter number of centuries");
+        ulong century =  Convert.ToUInt64(Console.ReadLine());
+        ulong year = century * 100;
+        ulong day = year * 365;
+        ulong hours = day * 24;
+        ulong minutes = hours * 60;
+        ulong seconds = minutes * 60;
+        ulong milisec = seconds * 1000;
+        ulong microsec = milisec * 1000;
+        ulong nanosec = microsec * 1000;
+        Console.WriteLine($"{century} centuries = {year} years = {day} days = {hours} hours = {minutes} minutes = {seconds} seconds = {milisec} milliseconds = {microsec} microseconds = {nanosec} nanoseconds");
+
+    }
+    
+}
+
+public class PracticeLoops
+{
+    public void Question1()
+    {
+        int max = 500;
+        for (byte i = 0; i < max; i++)
+        {
+            if (i == 255)
+            {
+                Console.WriteLine("max reached");
+                break;
+            }
+            Console.WriteLine(i);
+        }
+    }
+
+    public void Question3GuessNumber()
+    {
+        Console.WriteLine("Guess a number between 1 and 3");
+        int correctNumber = new Random().Next(3) + 1;
+        int guessedNumber = int.Parse(Console.ReadLine());
+        while (correctNumber != guessedNumber)
+        {
+            if (guessedNumber > 3 || guessedNumber < 1)
+            {
+                Console.WriteLine("Your guess must be between 1 and 3");
+            }
+
+            else if (guessedNumber > correctNumber)
+            {
+                Console.WriteLine("Guess lower");
+            }
+            else if (guessedNumber < correctNumber)
+            {
+                Console.WriteLine("Guess higher");
+            }
+            guessedNumber = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("Bingo");
+        return;
+    }
+
+    public void Question2Pyramid(int width)
+    {
+        if (width % 2 == 0)
+        {
+            Console.WriteLine("width must be odd");
+            return;
+        }
+
+        int level = width / 2 + 1;
+        for (int i = 0; i < level; i++)
+        {
+            int spaces = (width - (1 + i * 2)) / 2;
+            for (int j = 0; j < spaces; j++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int j = 0; j < 1 + i * 2; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public void Question4Days(DateTime birthDay)
+    {
+        DateTime currentDay = DateTime.Today;
+        int days = (int) (currentDay - birthDay).TotalDays;
+        int daysToNextAnniversary = 10000 - (days % 10000); 
+        Console.WriteLine($"You are {days} days old, {daysToNextAnniversary} days to your next anniversary");
+    }
+
+    public void Question5Greeting()
+    {
+        int currentTime = DateTime.Now.Hour;
+        Console.WriteLine("The time is " + currentTime + " o'clock");
+        if (currentTime >= 0 && currentTime < 12)
+        {
+            Console.WriteLine("Good Morning");
+        }
+        else if (currentTime >= 12 && currentTime < 15)
+        {
+            Console.WriteLine("Good Afternoon");
+        }
+        else if (currentTime >= 15 && currentTime < 20)
+        {
+            Console.WriteLine("Good Evening");
+        }
+        else if (currentTime >= 20 && currentTime <= 24)
+        {
+            Console.WriteLine("Good Night");
+        }
+    }
+
+    public void Question6Count()
+    {
+        int limit = 24;
+        for (int i = 1; i < 5; i++)
+        {
+            for (int j = 0; j < limit + 1; j+=i)
+            {
+                Console.Write(j);
+                if (j == limit)
+                {
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.Write(",");
+                }
+            }
+            
+        }
+    }
+}
+
+public class Test
+{
+    public static void Main()
+    {
+        HackerName hackerName = new HackerName();
+        // hackerName.AskName();
+        PracticeNumbers practiceNumbers = new PracticeNumbers();
+        // practiceNumbers.Question1();
+        // practiceNumbers.Question2();
+        PracticeLoops practiceLoops = new PracticeLoops();
+        // practiceLoops.Question1();
+        // practiceLoops.Question3GuessNumber();
+        // practiceLoops.Question2Pyramid(9);
+        // practiceLoops.Question4Days( DateTime.Parse("1999/01/30"));
+        // practiceLoops.Question5Greeting();
+        // practiceLoops.Question6Count();
+        PracticeArrays practiceArrays = new PracticeArrays();
+        // practiceArrays.ArrayCopying();
+        // practiceArrays.GroceryList();
+        // PracticeArrays.FindPrimesInRange(1, 20);
+        // practiceArrays.CaculateSum();
+        // practiceArrays.FindLongest();
+        // practiceArrays.FindMostFrequency();
+        PracticeStrings practiceStrings = new PracticeStrings();
+        // practiceStrings.ReverseString();
+        practiceStrings.SplitAndReverse();
+    }
+    
+    
+    
+    
+    
+}
---
+++ ConsoleApp1/ProgramA2.cs
@@ -0,0 +1,292 @@
+using System.Collections;
+using System.Text;
+
+namespace ConsoleApp1;
+
+public class PracticeArrays
+{
+    public void ArrayCopying()
+    {
+        int[] s = new int[10];
+        for (int i = 0; i < s.Length; i++)
+        {
+            s[i] = i;
+        }
+
+        int[] d = new int[s.Length];
+        for (int i = 0; i < s.Length; i++)
+        {
+            d[i] = s[i];
+            Console.WriteLine($"{i}: {s[i]} is copied to {d[i]}");
+        }
+    }
+
+    public void GroceryList()
+    {
+        ArrayList list = new ArrayList(); 
+        do
+        {
+            Console.WriteLine("Enter command (+ item, - item, or -- to clear)):");
+            char symbol = (char) Console.Read();
+            if (symbol == '+')
+            {
+                list.Add(Console.ReadLine().Trim());
+            }
+            else if (symbol == '-')
+            {
+                char nextSymbol = (char)Console.Read();
+                if ( nextSymbol == '-')
+                {
+                    list = new ArrayList();
+                }
+                else
+                {
+                    list.Remove((nextSymbol + Console.ReadLine()).Trim());
+                }
+                
+            }
+            else
+            {
+                continue;
+            }
+            Console.WriteLine("----------Grocery List:-----------");
+            foreach (String line in list)
+            {
+                Console.WriteLine(line);
+            }
+            Console.WriteLine("----------------------------------");
+        } while (true);
+    }
+
+    public static bool isPrime(int num)
+    {
+        if (num == 1)
+            return false;
+        for (int j = 2; j < num; j++)
+        {
+            if (num % j == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static int[] FindPrimesInRange(int startNum, int endNum)
+    {
+        int[] results = new int[endNum - startNum + 1];
+        int lastIndex = 0;
+
+        for (int i = startNum; i <= endNum; i++)
+        {
+            if (isPrime(i))
+            {
+                results[lastIndex++] = i;
+            }
+        }
+        
+        int[] truncResults = new int[lastIndex];
+        Array.Copy(results, truncResults, lastIndex);
+        Console.WriteLine(string.Join(", ", truncResults));
+        return truncResults;
+    }
+
+    public int[] EnterArray()
+    {
+        Console.WriteLine("Enter array:");
+        string arrayStringInput = Console.ReadLine();
+        string[] arrayString = arrayStringInput.Split(' ');
+        int[] arrayInt = new int[arrayString.Length];
+        for (int i = 0; i < arrayString.Length; i++)
+        {
+            arrayInt[i] = Int32.Parse(arrayString[i]);
+        }
+
+        return arrayInt;
+    }
+
+    public void CaculateSum()
+    {
+        int[] arrayInt = EnterArray();
+        
+        int[] sumArray = new int[arrayInt.Length];
+        Console.WriteLine("Enter K:");
+        int kInput = Int32.Parse(Console.ReadLine());
+        
+        for (int i = 0; i < sumArray.Length; i++)
+        {
+            int sum = 0;
+            for (int j = 1; j <= kInput; j++)
+            {
+                int index;
+                if ((i - j) % arrayInt.Length < 0)
+                {
+                    index = arrayInt.Length + (i - j) % arrayInt.Length;
+                }
+                else
+                {
+                    index = (i - j) % arrayInt.Length;
+                }
+                // Console.Write(i + "-" + j + " " + index + " ");
+                sum += arrayInt[index];
+                // Console.Write(arrayInt[index] + " ");
+            }
+            Console.WriteLine();
+            sumArray[i] = sum;
+        }
+        
+        Console.WriteLine(string.Join(" ", sumArray));
+    }
+
+    public void FindLongest()
+    {
+        int[] arrayInt = EnterArray();
+
+        int longest = 1;
+        int[] longestSub = new int[arrayInt.Length];
+        longestSub[0] = 1;
+        int longestInt = arrayInt[0];
+        
+        for (int i = 1; i < arrayInt.Length; i++)
+        {
+            if (arrayInt[i] == arrayInt[i-1])
+            {
+                longestSub[i] = longestSub[i - 1] + 1;
+                if (longestSub[i] > longest)
+                {
+                    longestInt = arrayInt[i];
+                    longest = longestSub[i];
+                }
+            }
+            else
+            {
+                longestSub[i] = 1;
+            }
+        }
+
+        for (int i = 0; i < longest; i++)
+        {
+            Console.Write(longestInt + " ");
+        }
+        Console.WriteLine();
+    }
+
+    public void FindMostFrequency()
+    {
+        int[] arrayInt = EnterArray();
+        Dictionary<int, int> freq = new Dictionary<int, int>();
+
+        for (int i = 0; i < arrayInt.Length; i++)
+        {
+            if (freq.ContainsKey(arrayInt[i]))
+            {
+                freq[arrayInt[i]] += 1;
+            }
+            else
+            {
+                freq.Add(arrayInt[i], 1);
+            }
+        }
+
+        List<int> freqValues = freq.Values.ToList();
+        freqValues.Sort();
+        freqValues.Reverse();
+        int mostFreq = freqValues[0];
+        List<KeyValuePair<int, int>> mostFreqPairs = new List<KeyValuePair<int, int>>();
+        foreach (var pair in freq)
+        {
+            int value = pair.Value;
+            if (value == mostFreq)
+            {
+                mostFreqPairs.Add(pair);
+            }
+        }
+
+        if (mostFreqPairs.Count == 1)
+        {
+            Console.WriteLine($"The number {mostFreqPairs[0].Key} is the most frequent (occurs {mostFreqPairs[0].Value} times)");
+        }
+        else
+        {
+            Console.Write("The numbers ");
+            for (int i = 0; i < mostFreqPairs.Count; i++)
+            {
+                Console.Write(mostFreqPairs[i].Key);
+                if (i == mostFreqPairs.Count - 2)
+                {
+                    Console.Write(" and " + mostFreqPairs[i+1].Key);
+                    break;
+                }
+                else
+                {
+                    Console.Write(", ");
+                }
+            }
+            Console.WriteLine($" have the same maximal frequency (each occurs {mostFreqPairs[0].Value} times). The leftmost of them is {mostFreqPairs[0].Key} .");
+        }
+    }
+}
+
+public class PracticeStrings
+{
+    public void ReverseString()
+    {
+        string readInput = Console.ReadLine();
+        char[] chars = readInput.ToCharArray();
+        Array.Reverse(chars);
+        string output = new string(chars);
+        Console.WriteLine("method1: " + output);
+        Console.Write("method2: ");
+        for (int i = readInput.Length-1; i >= 0; i--)
+        {
+            Console.Write(readInput[i]);
+        }
+        Console.WriteLine();
+    }
+
+    public void SplitAndReverse()
+    {
+        string readInput = Console.ReadLine();
+        char[] separator = new[]
+        {
+            ',',
+            '.',
+            ';',
+            ':',
+            '=',
+            '(',
+            ')',
+            '&',
+            '[',
+            ']',
+            '"',
+            '\'',
+            '\\',
+            '/',
+            '!',
+            '?',
+            ' '
+        };
+        string[] tokens = readInput.Split(separator);
+        Console.WriteLine(string.Join(",", tokens));
+        List<int> pos = new List<int>();
+        List<char> sym = new List<char>();
+        int index = 0;
+        for (int i = 0; i < tokens.Length; i++)
+        {
+            if (tokens[i] == "")
+            {
+                Console.WriteLine(i + " " + readInput[index]);
+                pos.Add(i);
+                sym.Add(readInput[index]);
+                index++;
+            }
+            else
+            {
+                index += tokens[i].Length + 1;
+                Console.WriteLine(i + " " + index);
+            }
+        }
+    }
+}
